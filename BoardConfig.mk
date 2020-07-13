7df412d926a8e2f6fe809a28f277c55ddd30367b
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-12 23:36:23 -0400

    raphael: Kill deprecated/unneeded board flags
    * BOARD_KERNEL_TAGS_OFFSET  - Not used anywhere.
    * BOARD_RAMDISK_OFFSET - Not used anywhere.
    * TARGET_KERNEL_ARCH - no-op unless it differs from TARGET_ARCH.
    
    Change-Id: I8d140fecdbbaac979bc8398745ffa95b911e4c89

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,9 +40,6 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive kpti=off
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_KERNEL_ARCH := arm64
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
