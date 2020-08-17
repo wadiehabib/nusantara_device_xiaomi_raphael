01e8208bca2c71a6f494914affbc1b17838ee848
PIPIPIG233666 <pig.priv@gmail.com>
2020-08-17 11:30:18 +0530

    raphael: Enable TARGET_USES_FOD_ZPOS
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,7 @@ BOARD_HAVE_QCOM_FM := true
 
 # FOD
 TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.raphael
+TARGET_USES_FOD_ZPOS := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
