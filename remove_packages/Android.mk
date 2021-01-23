9a77a897be01caa09b1027c68a770d3c200cbe39
Michael Bestas <mkbestas@lineageos.org>
2021-01-23 20:04:06 +0900

    raphael: Move FM libs to /system_ext

---
+++ remove_packages/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AppDirectedSMSService CarrierSetup ConnMO DCMO Drive Maps MyVerizonServices OBDM_Permissions OemDmTrigger PrebuiltGmail SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger YouTube YouTubeMusicPrebuilt obdm_stub
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
