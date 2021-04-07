d0a74601e97bbd796a31b1622e4733554c12ed8c
stallix <joey@evolution-x.org>
2021-04-08 01:28:53 +0300

    raphael: enable iorap prefetch/address denials

--- system_prop.mk
+++ system_prop.mk
@@ -131,3 +131,7 @@ PRODUCT_PROPERTY_OVERRIDES += debug.sf.early.sf.duration=21000000
 PRODUCT_PROPERTY_OVERRIDES += debug.sf.early.app.duration=16500000
 PRODUCT_PROPERTY_OVERRIDES += debug.sf.earlyGl.sf.duration=13500000
 PRODUCT_PROPERTY_OVERRIDES += debug.sf.earlyGl.app.duration=21000000
+
+# Iorap
+iorapd.perfetto.enable=true
+iorapd.readahead.enable=true
