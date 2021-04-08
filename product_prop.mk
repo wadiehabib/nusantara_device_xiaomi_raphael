d27db9975b2f5630d9d2c9eabe05c5c59fb379bd
Wadie Habib <77555267+mans0amador@users.noreply.github.com>
2021-04-09 01:35:13 +0300

    raphael: enable usb debuggging by default in eng

--- product_prop.mk
+++ product_prop.mk
@@ -9,8 +9,10 @@ ifeq ($(TARGET_BUILD_VARIANT),eng)
 # /vendor/default.prop is force-setting ro.adb.secure=1
 # Get rid of that by overriding it in /product on eng builds
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.secure=0 \
-    ro.adb.secure=0
+     persist.sys.usb.config=mtp,adb \
+     ro.adb.secure=0 \
+     ro.secure=0 \
+     ro.debuggable=1
 endif
 
 # Dex2oat
