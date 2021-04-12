ce257b148b140efd9fd1a3770d12e7d717661f78
TheScarastic <warabhishek@gmail.com>
2021-04-12 04:24:07 +0300

    raphael: ignore priv-app permissions

--- product_prop.mk
+++ product_prop.mk
@@ -27,3 +27,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.debug.wfd.enable=0
 
+#Priv-app permission
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.control_privapp_permissions=log
