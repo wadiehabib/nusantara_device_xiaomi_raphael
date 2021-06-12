be64ebc7d0108b2fd7279e7d5914c8f65701c4e1
Astile97 <zilliondollarkuruvi@gmail.com>
2021-06-12 12:03:54 +0530

    raphael: parts: ThermalUtils startService -> initialize
    device/xiaomi/surya/parts/src/org/lineageos/settings/BootCompletedReceiver.java:45: error: startService(Context) has protected access in ThermalUtils:  ThermalUtils.startService(context);
    
    Signed-off-by: agustkid <agustkid@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -38,6 +38,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
         PopupCameraUtils.startService(context);
-        ThermalUtils.startService(context);
+        ThermalUtils.initialize(context);
     }
 }
