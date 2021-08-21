a1ba7678b5c5013bb42d22b726307d62c1fe0ad5
cjh1249131356 <cjh1249131356@gmail.com>
2021-08-21 05:46:52 -0700

    Raphael: Build mock powerstat hal

--- device.mk
+++ device.mk
@@ -346,7 +346,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+		android.hardware.power.stats@1.0-service.mock
 
 # QMI
 PRODUCT_PACKAGES += \
