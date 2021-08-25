afd2d1d005edd01719dcf63cb643915f1dfa1122
mans0amador <wadie.habib@live.com>
2021-08-26 02:29:58 +0300

    Nusantara bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_raphael.mk
+    $(LOCAL_DIR)/nad_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_raphael-user \
-    ancient_raphael-userdebug \
-    ancient_raphael-eng
+    nad_raphael-user \
+    nad_raphael-userdebug \
+    nad_raphael-eng
--- nad_raphael.mk
+++ nad_raphael.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,29 +13,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := ancient_raphael
-PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := nad_raphael
 
 PRODUCT_CHARACTERISTICS := nosdcard
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
-
-# Ancient stuff
+NAD_BUILD_TYPE := OFFICIAL
+#USE_GAPPS ?= true
+#TARGET_GAPPS_ARCH := arm64
+TARGET_FOD_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
-EXTRA_FOD_ANIMATIONS := true
-
-# Ancient
-ANCIENT_OFFICIAL=true
-#TARGET_GAPPS_ARCH := arm64
-#IS_PHONE := true
-#ANCIENT_GAPPS := true
-#ANCIENT_WEEABO := true
+USE_PIXEL_CHARGING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
