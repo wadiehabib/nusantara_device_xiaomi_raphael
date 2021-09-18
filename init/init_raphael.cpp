65c2a5d3ed405599bf426e2c1265503a6cdca0f4
wadiehabib <wadie4jesus@gmail.com>
2021-09-18 14:59:11 +0000

    raphael: update to Redfin September FP

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -90,8 +90,8 @@ void set_device_props(const std::string fingerprint, const std::string descripti
 
 void vendor_load_properties() {
     // SafetyNet madness
-    char const fp[] = "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys";
-    char const fp_desc[] = "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys";
+    char const fp[] = "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys";
+    char const fp_desc[] = "redfin-user 11 RQ3A.210905.001 7511028 release-keys";
 
     std::string region = android::base::GetProperty("ro.boot.hwc", "");
 
