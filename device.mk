5c2ce004f6e9e14fa46c7e9fad301c82ea59fa46
pavanpaps <pavanishot200@gmail.com>
2021-09-27 17:39:31 +0000

    [WIP] raphael: Prebuilt Cam Provider

--- device.mk
+++ device.mk
@@ -153,13 +153,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    libcamera2ndk_vendor \
     libdng_sdk.vendor \
-    libgui_vendor \
     Snap \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
 # Component overrides
